fcf22533678e45c42c7eb65ae6a18e50c7666487
binoy-d-shah <binoy.d.shah@gmail.com>
2025-04-04 14:42:45 +0200

    Update config.ts

--- utils/config.ts
+++ utils/config.ts
@@ -4,5 +4,5 @@ dotenv.config();
 
 export const config = {
     baseUrl: process.env.BASE_URL || 'https://magento.softwaretestingboard.com/rest/default/V1',
-    adminToken: process.env.ADMIN_TOKEN || 'cdxf96vcah8pbivc3zzrshd8t15ikua5',
+    adminToken: process.env.ADMIN_TOKEN || 'your_admin_token_here',
 };
